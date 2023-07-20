71b3b64e5a4ded267c822b668718d74b9cd12d05
NicolasGreg <nicoe_97@live.com>
2023-07-20 08:58:03 -0300

    update package.json vite config

--- src/components/FotoPrincipal/Foto2.jsx
+++ src/components/FotoPrincipal/Foto2.jsx
@@ -3,7 +3,7 @@ import { Box,Image } from "@chakra-ui/react"
 
 function Foto2(){
     return(
-        <Image w={{ base: "94%", lg: "36%" }} h={{ base: "auto", lg: "56.25rem" }}  mr={{ base: "0%", lg: "3%" }} mt={{base:"-88%", lg:"0"}}  src='..foto2.jpeg' alt='Nicolas' objectFit={"cover"}
+        <Image w={{ base: "94%", lg: "36%" }} h={{ base: "auto", lg: "56.25rem" }}  mr={{ base: "0%", lg: "3%" }} mt={{base:"-88%", lg:"0"}}  src='foto2.jpeg' alt='Nicolas' objectFit={"cover"}
          display={{ lg: "block" }} />
     )
 }
