48417fe8f21a320a514b304f83d922cb97baa5cb
NicolasGreg <nicoe_97@live.com>
2023-07-20 08:54:15 -0300

    update package.json vite.config

--- src/components/FotoPrincipal/FotoPrincipal.jsx
+++ src/components/FotoPrincipal/FotoPrincipal.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Box,Image } from "@chakra-ui/react"
 function FotoPrincipal(){
     return(
-        <Image w={{base:"100%", lg:"100%"}} h={{base:"auto", lg:'100vh'}}  mr={{base:"0",lg:"3%"}} src='../nico.jpeg' alt='Nicolas' display={{ lg: "block" }}/>
+        <Image w={{base:"100%", lg:"100%"}} h={{base:"auto", lg:'100vh'}}  mr={{base:"0",lg:"3%"}} src='..nico.jpeg' alt='Nicolas' display={{ lg: "block" }}/>
     )
 }
 export{FotoPrincipal}
